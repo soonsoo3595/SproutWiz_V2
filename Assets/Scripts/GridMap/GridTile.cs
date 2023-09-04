8ec4240ec124581acd7baa5f703044038f2f7280
RePro1202 <astra852456@gmail.com>
2023-09-04 22:38:43 +0900

    Add GridTile.cs
    프로토타입의 초기 Node.cs처럼 GridManager에서 생성하는 맵의 개별 타일 데이터를 가지고 있는 클래스.

---
+++ Assets/Scripts/GridMap/GridTile.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile
+{
+    private GridPosition gridPosition;
+    private List<TileUnit> units;
+
+    public GridTile(GridPosition gridPosition)
+    {
+        this.gridPosition = gridPosition;
+        units = new List<TileUnit>();
+    }
+
+    public override string ToString()
+    {
+        if (units.Count == 0)
+        {
+            return gridPosition.ToString();
+        }
+        else
+        {
+            string unitString = "";
+
+            foreach (TileUnit unit in units)
+            {
+                unitString += unit + "\n";
+            }
+
+            return gridPosition.ToString() + "\n" + unitString;
+        }
+    }
+
+    public void AddUnit(TileUnit unit)
+    {
+        units.Add(unit);
+    }
+
+    public void RemoveUnit(TileUnit unit)
+    {
+        units.Remove(unit);
+    }
+
+    public List<TileUnit> GetUnitList()
+    {
+        return units;
+    }
+}
