2a7fedc3852c0b29697e9344cefc3f50b86f3e18
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-09-19 17:44:54 +0900

    타일 데이터 관리
    Element와 GrowthStep으로 growPoint 관리

--- Assets/Scripts/TileData.cs
+++ Assets/Scripts/TileData.cs
@@ -1,16 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
-
-
-public enum Element
-{
-    None,
-    Fire,
-    Warter,
-    Grass
-}
-
 
 public class TileData
 {
@@ -19,17 +10,74 @@ public class TileData
     public int growPoint;
     public int maxGrowPoint;
     public Element element;
+    public GrowthStep step;
 
     public TileData(GridPosition gridPosition)
     {
         this.gridPosition = gridPosition;
         this.growPoint = 0;
-        this.maxGrowPoint = 0;
-        this.element = Element.None;
+        this.maxGrowPoint = 6;  // 나중에 maxGrowPoint를 따로 빼서 하드코딩 안하게
+        this.element = new Element();
+        this.step = GrowthStep.Empty;
     }
 
     public override string ToString()
     {
-        return element.ToString() + '\n' + growPoint.ToString();
+        return element.ToString() + '\n' + growPoint.ToString() + '\n' + step.ToString();
+    }
+
+    public void InitTile()
+    {
+        growPoint = 0;
+        step = GrowthStep.Empty;
+    }
+
+    public void SetData(TetrisUnit unit)
+    {
+        if (step == GrowthStep.Empty)
+        {
+            element = unit.GetElement();
+            growPoint++;
+        }
+        else
+        {
+            switch (element.GetElementRelation(unit.GetElement()))
+            {
+                case ElementRelation.Advantage:
+                    growPoint = Mathf.Clamp(growPoint + unit.GetGrowPoint() * 2, 0, maxGrowPoint);
+                    break;
+                case ElementRelation.Equal:
+                    growPoint = Mathf.Clamp(growPoint + unit.GetGrowPoint(), 0, maxGrowPoint);
+                    break;
+                case ElementRelation.Disadvantage:
+                    growPoint = 0;
+                    break;
+            }
+        }
+
+        UpdateTile();
+    }
+
+    // 변경 필요!!!!!
+    public void UpdateTile()
+    {
+        if(growPoint == 0)
+        {
+            InitTile();
+        }
+        else if(growPoint == 1)
+        {
+            step = GrowthStep.Sprout;
+        }
+        else if(growPoint > 1 && growPoint < maxGrowPoint)
+        {
+            step = GrowthStep.GrowUp;
+        }
+        else if(growPoint == maxGrowPoint)
+        {
+            step = GrowthStep.Harvest;
+            InitTile();
+        }
+        
     }
 }
