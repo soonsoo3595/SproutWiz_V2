7fd933f7ed8e5d8fd3e6b66edfd22862a7d14017
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-10-06 22:50:23 +0900

    기획 변경으로 인한 StageInfo 삭제

--- Assets/Jaehoon/StageInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptables/StageInfo")]
-public class StageInfo : ScriptableObject
-{
-    public List<int> targets;
-}
