dc8b467ba016fca01ca0a8f1e7ff4d94c345e92d
RePro1202 <astra852456@gmail.com>
2023-11-01 20:49:35 +0900

    스프라이트 관리용 ScriptableObject 추가.

---
+++ Assets/Scripts/GridMap/SpriteSetting.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpriteSetting", menuName = "MyGame/Sprite Settings", order = 2)]
+public class SpriteSetting : ScriptableObject
+{
+    public Sprite Seed;
+
+    public Sprite FireGrowth;
+    public Sprite WaterGrowth;
+    public Sprite GrassGrowth;
+
+    //public Sprite FireHarvest;
+    //public Sprite WaterHarvest;
+    //public Sprite GrassHarvest;
+}
