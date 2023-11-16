3b39f15cbfe4a1e0b6214cda521282a60e6c1a5e
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-11-16 22:11:43 +0900

    멀티 수확 시 화면 텍스트 출력
    후에 폰트 바꿔야 함

--- Assets/Scripts/MainGame/ScoreSystem.cs
+++ Assets/Scripts/MainGame/ScoreSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +9,8 @@ public class ScoreSystem : MonoBehaviour
     private int score;
 
     public MainGame mainGame;
+    public GameObject combo;
     public Text scoreTxt;
-
 
     [Header("Score")]
     public int normalHarvestScore = 100;
@@ -45,7 +46,34 @@ public class ScoreSystem : MonoBehaviour
         int curScore = normalHarvestScore * count + multiHarvestScore[count];
 
         Debug.Log(count + "°³ ¼öÈ®ÇØ¼­ " + curScore + "Á¡ È¹µæ");
+        StartCoroutine(PrintCombo(count));
         AddScore(curScore);
         mainGame.feverSystem.IncreaseGauge(count);
+    }
+
+    IEnumerator PrintCombo(int count)
+    {
+        if (count <= 1) yield break;
+
+        combo.SetActive(true);
+        
+        Text comboTxt = combo.GetComponentInChildren<Text>();
+
+        switch(count)
+        {
+            case 2:
+                comboTxt.text = "Good!";
+                break;
+            case 3:
+                comboTxt.text = "Nice!";
+                break;
+            case 4:
+                comboTxt.text = "Excellent!";
+                break;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        combo.SetActive(false);
     }
 }
