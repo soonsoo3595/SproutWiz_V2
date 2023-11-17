99c0e6cb4b69cdd64ae29c42ceabe775e7ab3655
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-11-18 02:22:33 +0900

    빈 클래스 생성

---
+++ Assets/Jaehoon/PlayerData.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+
+
+    int totalScore;    // 누적 스코어
+    int bestScore;      // 최고 기록 스코어
+
+    int gold;           // 보유 골드
+
+    int harvestCount;   // 수확한 총 갯수
+    int multiHarvestCount;  // 멀티 수확한 총 갯수
+    int achieveGoalCount;   // 목표 달성한 총 갯수
+    int feverCount;         // 피버 모드 진입한 총 횟수
+    int reRollCount;        // 리롤 횟수
+}
