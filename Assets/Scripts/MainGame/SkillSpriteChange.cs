b397ca97be8ede0242b69522a2d29dd40fe2b129
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-11-27 20:01:33 +0900

    리롤, 피버 버튼 클릭 시 스프라이트 교체

---
+++ Assets/Scripts/MainGame/SkillSpriteChange.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SkillSpriteChange : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private Button button;
+
+    public Sprite beforeClick;
+    public Sprite afterClick;
+    public Image skillpad;
+    public GameObject bar, icon;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(button.interactable == false) return;
+
+        bar.SetActive(false); icon.SetActive(false);
+        skillpad.sprite = afterClick;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (button.interactable == false) return;
+
+        skillpad.sprite = beforeClick;
+        bar.SetActive(true); icon.SetActive(true);
+    }
+}
