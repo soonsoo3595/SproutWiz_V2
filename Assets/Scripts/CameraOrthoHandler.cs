5e2a869e8972a9363fef3bc5b78c1bf0174c50f2
RePro1202 <78546867+RePro1202@users.noreply.github.com>
2023-12-13 20:03:38 +0900

    중간UI 위치 자동 조절 기능 추가.
    1. 화면 세이프 에리어 값에 따라 위치 변경.
    2. 현재와 앞선 기능 모두 갤럭시 폴드 2에서 이상 확인. 수정 필요.

--- Assets/Scripts/CameraOrthoHandler.cs
+++ Assets/Scripts/CameraOrthoHandler.cs
@@ -8,20 +8,40 @@ public class CameraOrthoHandler : MonoBehaviour
     readonly float screenRatiolimit = 2.7f;
     readonly float gridmapWorldSize = 5f;
 
+    [SerializeField] RectTransform middleUI;
     CinemachineVirtualCamera virtualCamera;
 
     void Awake()
     {
+        // 카메라 배율 조정.
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
 
-        float screenWidth = Screen.width;
-        float screenHeight = Screen.height;
-
-        float screenRatio = (float)screenHeight / screenWidth;
+        float screenRatio = (float)Screen.height / Screen.width;
 
         if (screenRatio < screenRatiolimit)
         {
             virtualCamera.m_Lens.OrthographicSize = (gridmapWorldSize * screenRatiolimit) / 2;
         }
+
+
+        // MiddleUI 위치 조정.
+        Rect safeArea = Screen.safeArea;
+
+        float separationY = Screen.height - (safeArea.height - safeArea.y);
+
+        Vector3 separationWorldSpace =
+            Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f))
+            - Camera.main.ScreenToWorldPoint(new Vector3(0f, safeArea.height - safeArea.y, 0f));
+
+
+        if(separationY > 0)
+            middleUI.position = new Vector3(middleUI.position.x, middleUI.position.y + separationWorldSpace.y / 2, middleUI.position.z);
+
+
+        Debug.Log($"screenRatio: {screenRatio}");
+        Debug.Log($"screenWidth : {Screen.width}, screenHeight : {Screen.height}");
+        Debug.Log($"Safe Area: {safeArea}");
+        Debug.Log($"separationWorldSpaceY : {separationWorldSpace.y}");
+        Debug.Log($"separationY : {separationY}");
     }
 }
